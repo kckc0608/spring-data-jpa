27ddddb66f0a3fc8289cc6951061db998130101e
kwon <kckc0608@naver.com>
2024-03-21 22:59:14 +0900

    [Section 1] Spring Data JPA 사용해보기

---
+++ src/test/java/study/datajpa/repository/MemberDataJpaRepositoryTest.java
@@ -0,0 +1,33 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberDataJpaRepositoryTest {
+    @Autowired
+    MemberDataJpaRepository memberDataJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("kim");
+        memberDataJpaRepository.save(member);
+
+        Member savedMember = memberDataJpaRepository.save(member);
+
+        Member findMember = memberDataJpaRepository.findById(savedMember.getId()).get();
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
+}
